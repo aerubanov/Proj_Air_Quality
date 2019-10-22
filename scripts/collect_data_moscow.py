f9b86795e3c1218dc74c55358ec7222d30261942
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-10-22 13:48:50 +0300

    add docstring and comments

--- scripts/collect_data_moscow.py
+++ scripts/collect_data_moscow.py
@@ -1,9 +1,9 @@
 import datetime
 import requests
 import os
-from threading import Thread
 import time
 
+# filename patterns for specifying sensors types
 DATA_FOLDER = '../DATA/'
 SERVER_URL = 'http://archive.luftdaten.info/'
 BME280 = '_bme280_sensor_'
@@ -21,25 +21,36 @@ PMS7003 = '_pms7003_sensor_'
 PPD42NS = '_ppd42ns_sensor_'
 SDS011 = '_sds011_sensor_'
 
+# list of SDS011 particle concentration sensors in Moscow on 18.10.2019
 pm_sensor_id_list = [26014, 26251, 24797, 26219, 31766, 28509, 26191, 32709, 20219, 30289, 20225, 22314, 24028,
                      24088, 25083, 23200, 23294, 28802, 25021, 28401, 19836, 32465, 19649, 24026, 25561, 27418,
                      31668, 32487, 32226, 32403, 23150, 24309, 32756, 32802, 19838, 20229, 23512, 22044, 32439,
                      32772, 32866, 22632, 24052, 26058, 27213, 23317, 24687, 28286, 30669, 31087, 31862, 30810,
                      30920, 31174, 31650, 32074, 24997, 30529, 30731, 30862, 32337, 32407]
-
+# list of BME280 temperature and humidity sensor in Moscow on 18.10.2019
 bme_temp_sensor_id_list = [31767, 26220, 32791, 28510, 26192, 24029, 25084, 23201, 23295, 31669, 32227, 32488,
                            24027, 32466, 32404, 32867, 32440, 20228, 23516, 22045, 26059, 27214, 24688, 31175,
                            23318, 31088, 24578, 30530, 30732, 30905, 30921, 32075, 32338, 24998]
-
+# list of DHT22 temperature and humidity sensor in Moscow on 18.10.2019
 dht_temp_sensor_id_list = [30351, 28905, 22633]
 
 
 def construct_url(sensor_id, date, sensor_type):
+    """generate url for download csv file with data by date"""
     url = SERVER_URL + str(date) + '/' + str(date) + sensor_type + str(sensor_id) + '.csv'
     return url
 
 
 def download_data_for_interval(start_date, num_days, filename, sensor_id, sensor_type):
+    """
+    Download all data for time interval and save in one file
+    :param start_date: start point of time interval
+    :param num_days: number of days from start_date to download data
+    :param filename: filename to save results
+    :param sensor_id: id of sensor
+    :param sensor_type: type of sensor
+    :return: None
+    """
     dates = [start_date + datetime.timedelta(days=1) * i for i in range(num_days)]
     header_writen = False
     with open(os.path.join(DATA_FOLDER, filename), 'w') as f:
