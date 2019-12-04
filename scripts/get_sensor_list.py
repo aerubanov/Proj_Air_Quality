6ab1f8c31a0a8462cf09daa1b6edf10a6a39da25
Anatoly <anatolijrubanov@gmail.ru>
2019-12-04 18:09:08 +0300

    get filenames as parameter

--- scripts/get_sensor_list.py
+++ scripts/get_sensor_list.py
@@ -9,16 +9,19 @@ MAX_LAT = 56.47
 MIN_LON = 36.82
 MAX_LON = 38.40
 
+CHECKED_LINKS_FILE = 'links_list.txt'
+SENSOR_ID_FILE = 'sensorID_list.txt'
+
 SERVER_URL = 'http://archive.luftdaten.info/'
 
 
-def get_links(for_date):
+def get_links(for_date, links_file):
     try:
-        fh = open('links_list.txt', "r")
+        fh = open(links_file, "r")
     except FileNotFoundError:
-        fh = open('links_list.txt', "w")
+        fh = open(links_file, "w")
     fh.close()
-    with open('links_list.txt', 'r') as links_file:
+    with open(links_file, 'r') as links_file:
         checked_links = links_file.read().splitlines()
         checked_links = [i[10:] for i in checked_links]
         url = SERVER_URL + str(for_date) + '/'
@@ -30,16 +33,16 @@ def get_links(for_date):
     return links
 
 
-def check_sensor_pos(links):
+def check_sensor_pos(links, links_file, sensor_id_file):
     id_list = []
     try:
-        fh = open('sensorID_list.txt', "r")
+        fh = open(sensor_id_file, "r")
     except FileNotFoundError:
-        fh = open('sensorID_list.txt', "w")
+        fh = open(sensor_id_file, "w")
     fh.close()
     for url in links:
-        with open('links_list.txt', 'a') as links_file, requests.Session() as s,\
-                open('sensorID_list.txt', 'a') as s_file:
+        with open(links_file, 'a') as l_file, requests.Session() as s,\
+                open(sensor_id_file, 'a') as s_file:
             download = s.get(url)
             decoded_content = download.content.decode('utf-8')
             cr = csv.DictReader(decoded_content.splitlines(), delimiter=';')
@@ -49,7 +52,7 @@ def check_sensor_pos(links):
                 lat = float(row['lat'])
                 lon = float(row['lon'])
                 fname = url.split("/")[-1]
-                links_file.write(fname+'\n')
+                l_file.write(fname+'\n')
                 if MIN_LAT <= lat <= MAX_LAT and MIN_LON <= lon <= MAX_LAT:
                     id_list.append(fname[10:])
                     s_file.write(fname[10:]+'\n')
@@ -60,7 +63,7 @@ def check_sensor_pos(links):
 
 
 if __name__ == '__main__':
-    date = datetime.datetime(2019, 12, 1)
-    ln = get_links(date.date())
-    sensor_list = check_sensor_pos(ln)
+    date = datetime.datetime(2019, 12, 3)
+    ln = get_links(date.date(), CHECKED_LINKS_FILE)
+    sensor_list = check_sensor_pos(ln, CHECKED_LINKS_FILE, SENSOR_ID_FILE)
     print(sensor_list)
