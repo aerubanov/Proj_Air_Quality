86b6c78b4881e6925ee70edd58d70e7e8662fc90
Anatoly <anatolijrubanov@gmail.ru>
2019-12-05 18:15:44 +0300

    add update_data

---
+++ tests/test_update_data.py
@@ -0,0 +1,31 @@
+import datetime
+
+from scripts.config import SERVER_URL
+from scripts.update_data import construct_url, check_file
+
+
+def test_construct_url():
+    sensor_id = 111
+    sensor_type = 'type'
+    date = datetime.datetime(2019, 12, 5).date()
+    result = construct_url(sensor_id, date, sensor_type)
+    assert result == SERVER_URL + '2019-12-05/2019-12-05_type_sensor_111.csv'
+
+
+def test_check_file(tmpdir):
+    dir_name = 'test_data'
+    d = tmpdir.mkdir(dir_name)
+
+    res = check_file('some_file.csv', data_folder=dir_name)
+    assert res == (False, None)
+
+    f1 = d.join("empty.csv")
+    res = check_file(f1, data_folder=dir_name)
+    assert res == (False, None)
+
+    f2 = d.join("data.csv")
+    f2.write("sensor_id;sensor_type;location;lat;lon;timestamp;P1;durP1;ratioP1;P2;durP2;ratioP2\n"
+             "19649;SDS011;9973;55.686;37.589;2019-01-10T19:05:56;15.77;;;7.95;;\n"
+             "19649;SDS011;12232;55.728;37.476;2019-11-03T23:58:34;5.63;;;4.70;;\n")
+    res = check_file(f2, data_folder=dir_name)
+    assert res == (True, datetime.datetime(2019, 11, 3).date())
