acf4a8e23f16bf9530df1235c602f3c6842f4e98
Anatoly Rubanov <anatolijrubanov@gmail.com>
2019-12-10 21:57:31 +0300

    add dvc server id updating stage

--- scripts/config.py
+++ scripts/config.py
@@ -3,8 +3,8 @@ MAX_LAT = 56.47
 MIN_LON = 36.82
 MAX_LON = 38.40
 
-CHECKED_LINKS_FILE = 'links_list.txt'
-SENSOR_ID_FILE = 'sensorID_list.txt'
+CHECKED_LINKS_FILE = 'processing_lists/links_list.txt'
+SENSOR_ID_FILE = 'processing_lists/sensorID_list.txt'
 
 SERVER_URL = 'http://archive.luftdaten.info/'
 DATA_FOLDER = '../DATA/'
