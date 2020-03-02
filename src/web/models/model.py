ad3319b434b59e42c1c31f034ab54b7b8b565bdf
aerubanov <anatolijrubanov@gmail.com>
2020-03-02 19:05:08 +0300

    add forecast and anomaly to schema

--- src/web/models/model.py
+++ src/web/models/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, Float, String, Integer,
+from sqlalchemy import Column, DateTime, Float, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
