98bfaade202643f9aa438ef9805676fee6cf32d2
Anatoly Rubanov <anatolijrubanov@gmail.com>
2020-04-21 18:35:06 +0300

    fix path

--- src/web/loader/config.py
+++ src/web/loader/config.py
@@ -1,4 +1,4 @@
-sensor_file = 'data/sensors.csv'  # file with information about sensors to data downloading
+sensor_file = 'DATA/processed/sensors.csv'  # file with information about sensors to data downloading
 api_url = 'https://data.sensor.community/static/v2/data.json'
 sensor_time_interval = 5  # interval for api requests in minutes
 
