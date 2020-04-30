de41018160195797327c7c814e2a519070169b0e
Anatoly Rubanov <anatolijrubanov@gmail.com>
2020-04-30 21:51:36 +0300

    add percentiles to dataset

--- src/data/create_dataset.py
+++ src/data/create_dataset.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import datetime
 import typing
+import numpy as np
 
 from src.data.config import SENSOR_ID_FILE, SENSOR_DATA_FOLDER, WATHER_FILE, WATHER_DATA_FOLDER
 
@@ -11,96 +12,114 @@ START_DATE = datetime.date(2019, 4, 1)  # start date for dataset creation
 END_DATE = datetime.date.today() - datetime.timedelta(days=1)  # end date for dataset reation
 
 
-def average_sensors(data_folder: str, start_date: datetime.date,
-                    end_date: datetime.date) -> typing.Tuple[pd.DataFrame, pd.DataFrame]:
+def combine_data(files_list: typing.List[str], column: str,
+                 start_date=START_DATE, end_date=END_DATE) -> pd.DataFrame:
     """
-    Average data from all sensors
-    :param start_date: dataset beginning
-    :param end_date: dataset end
-    :param data_folder: path to folder with raw sensor data
-    :return: (average sensor data, senors type and location)
+    combine data from different sensor files in one dataframe
+    :param files_list: paths to files
+    :param column: selected data column
+    :param start_date: first date in results dataframe
+    :param end_date: last date in results dataframe
+    :return: dataframe with data from all files in files_list
     """
-    files = os.listdir(data_folder)
-    sds_files = [i for i in files if 'sds011' in i]
-    bme_files = [i for i in files if 'bme280' in i]
-    with open(SENSOR_ID_FILE, 'r') as sf:
-        sensor_list = sf.read().splitlines()
-        indoor_sensors = [i for i in sensor_list if 'indoor' in i]
-    sds_files = [i for i in sds_files if i.split('_')[0] not in indoor_sensors]
-    bme_files = [i for i in bme_files if i.split('_')[0] not in indoor_sensors]
-
-    sensors_data = []
     idx = pd.date_range(start_date, end_date, freq='5T')
-    sds_data = pd.DataFrame(idx, columns=['date'])
-    sds_data = sds_data.set_index('date')
-    bme_data = pd.DataFrame(idx, columns=['date'])
-    bme_data = bme_data.set_index('date')
-    from pandas.errors import EmptyDataError
-    for f in bme_files:
+    data_comb = pd.DataFrame(index=idx)
+    for file in files_list:
         try:
-            data = pd.read_csv(os.path.join(data_folder, f), delimiter=';', parse_dates=['timestamp'], index_col=5)
-            s_id = data.iloc[0].sensor_id
-            s_type = data.iloc[0].sensor_type
-            lat = data.iloc[0].lat
-            lon = data.iloc[0].lon
+            local_data = pd.read_csv(file, delimiter=';', parse_dates=['timestamp'], index_col=5)
+            local_data = local_data[[column]]
+            local_data = local_data[str(start_date):].resample('5T').mean()
+            local_data = local_data.reindex(idx, fill_value=None)
+            data_comb[file] = local_data[column]
+        except (pd.errors.ParserError, pd.errors.EmptyDataError,
+                pd.core.groupby.groupby.DataError, KeyError) as e:
+            print(str(e), file)
+    return data_comb
+
+
+def filtered_average(row: pd.Series) -> float:
+    """filter outliers and calculate mean"""
+    data = row.values[~np.isnan(row.values)]
+    q10 = np.quantile(data, q=0.1)
+    q90 = np.quantile(data, q=0.9)
+    data = data[data < q90]
+    data = data[data > q10]
+    return data.mean()
+
+
+def calc_percentiles(data: pd.DataFrame, name: str) -> pd.DataFrame:
+    """
+    Calculate percentiles for each row in dataframe
+    :param data: input data
+    :param name: base name for columns in output
+    :return: new dataframe
+    """
+    result = pd.DataFrame(index=data.index)
+    result[name+'_p10'] = data.quantile(q=0.1, axis=1)
+    result[name+'_p25'] = data.quantile(q=0.25, axis=1)
+    result[name+'_p50'] = data.quantile(q=0.5, axis=1)
+    result[name+'_p75'] = data.quantile(q=0.75, axis=1)
+    result[name+'_p90'] = data.quantile(q=0.9, axis=1)
+    result[name] = data.mean(axis=1)
+    result[name+'_filtr_mean'] = data.dropna(how='all').apply(filtered_average, axis=1)
+    return result
+
+
+def get_file_list(data_folder: str, sensor_type: str):
+    files = os.listdir(data_folder)
+    sensor_list = [i for i in files if sensor_type in i]
+    with open(SENSOR_ID_FILE, 'r') as sf:
+        sensor_id_list = sf.read().splitlines()
+        indoor_sensors = [i for i in sensor_id_list if 'indoor' in i]
+    sensor_list = [i for i in sensor_list if i.split('_')[0] not in indoor_sensors]
+    sensor_list = [os.path.join(data_folder, i) for i in sensor_list]
+    return sensor_list
+
+
+def get_sensors_loc(sensors_list: typing.List[str]) -> pd.DataFrame:
+    """
+    get location of sensors from sensors_list
+    :param sensors_list: list of sensor file paths
+    :return: dataframe with sensor locations
+    """
+    sensors_data = []
+    for i in sensors_list:
+        try:
+            data = pd.read_csv(i, delimiter=';')
+            s_id = data.iloc[-1].sensor_id
+            s_type = data.iloc[-1].sensor_type
+            lat = data.iloc[-1].lat
+            lon = data.iloc[-1].lon
             sensors_data.append([s_id, s_type, lat, lon])
-            data['pressure'] = data.pressure.replace('unavailable', None)
-            data['temperature'] = data.temperature.replace('unavailable', None)
-            data['humidity'] = data.humidity.replace('unavailable', None)
-            data['pressure'] = data.pressure.astype(float)
-            data['temperature'] = data.temperature.astype(float)
-            data['humidity'] = data.humidity.astype(float)
-            new_data = data['2019-04':].resample('5T').mean()
-            new_data.reindex(idx, fill_value=None)
-            bme_data['pressure_' + str(s_id)] = new_data.pressure
-            bme_data['temperature_' + str(s_id)] = new_data.temperature
-            bme_data['humidity_' + str(s_id)] = new_data.humidity
-        except EmptyDataError:
+        except (pd.errors.ParserError, pd.errors.EmptyDataError,
+                pd.core.groupby.groupby.DataError, KeyError):
             pass
-    from pandas.errors import EmptyDataError, ParserError
-    for f in sds_files:
-        try:
-            data = pd.read_csv(os.path.join(data_folder, f), delimiter=';', parse_dates=['timestamp'], index_col=5)
-            s_id = data.iloc[0].sensor_id
-            s_type = data.iloc[0].sensor_type
-            lat = data.iloc[0].lat
-            lon = data.iloc[0].lon
-            sensors_data.append([s_id, s_type, lat, lon])
-            data['P1'] = data.P1.replace('unavailable', None)
-            data['P2'] = data.P2.replace('unavailable', None)
-            data['P1'] = data.P1.astype(float)
-            data['P2'] = data.P2.astype(float)
-            new_data = data['2019-04':].resample('5T').mean()
-            new_data.reindex(idx, fill_value=None)
-            sds_data['P1_' + str(s_id)] = new_data.P1
-            sds_data['P2_' + str(s_id)] = new_data.P2
-        except (EmptyDataError, ParserError, KeyError) as e:
-            print(f, str(e))
-
-    sens = pd.DataFrame(sensors_data, columns=['sensor_id', 'sensor_type', 'lat', 'lon'])
-    p1_col = [i for i in sds_data.columns if 'P1_' in i]
-    p2_col = [i for i in sds_data.columns if 'P2_' in i]
-    temp_col = [i for i in bme_data.columns if 'temperature_' in i]
-    pres_col = [i for i in bme_data.columns if 'pressure_' in i]
-    hum_col = [i for i in bme_data.columns if 'humidity_' in i]
-
-    avg_data = pd.DataFrame(idx, columns=['date'])
-    avg_data = avg_data.set_index('date')
-    avg_data['P1'] = sds_data[p1_col].mean(axis=1, skipna=True)
-    avg_data['P2'] = sds_data[p2_col].mean(axis=1, skipna=True)
-    avg_data['pressure'] = bme_data[pres_col].mean(axis=1, skipna=True)
-    avg_data['temperature'] = bme_data[temp_col].mean(axis=1, skipna=True)
-    avg_data['humidity'] = bme_data[hum_col].mean(axis=1, skipna=True)
-    # avg_data['P1_std'] = sds_data[p1_col].std(axis=1, skipna=True)
-    # avg_data['P2_std'] = sds_data[p2_col].std(axis=1, skipna=True)
-    # avg_data['pressure_std'] = bme_data[pres_col].std(axis=1, skipna=True)
-    # avg_data['temperature_std'] = bme_data[temp_col].std(axis=1, skipna=True)
-    # avg_data['humidity_std'] = bme_data[hum_col].std(axis=1, skipna=True)
-    avg_data.index = avg_data.index.tz_localize(tz="UTC")
-    return avg_data, sens
+    sens_loc = pd.DataFrame(sensors_data, columns=['sensor_id', 'sensor_type', 'lat', 'lon'])
+    return sens_loc
 
 
-def get_wather_data(wather_file: str) -> pd.DataFrame:
+def get_sensor_data(data_folder: str) -> (pd.DataFrame, pd.DataFrame):
+    sds_files = get_file_list(data_folder, 'sds011')
+    bme_files = get_file_list(data_folder, 'bme280')
+
+    p1_data = combine_data(sds_files, 'P1')
+    p2_data = combine_data(sds_files, 'P2')
+    temp_data = combine_data(bme_files, 'temperature')
+    press_data = combine_data(bme_files, 'pressure')
+    hum_data = combine_data(bme_files, 'humidity')
+
+    perc_p1 = calc_percentiles(p1_data, 'P1')
+    perc_p2 = calc_percentiles(p2_data, 'P1')
+    perc_press = calc_percentiles(press_data, 'pressure')
+    perc_hum = calc_percentiles(hum_data, 'humidity')
+    perc_temp = calc_percentiles(temp_data, 'temperature')
+
+    sensor_data = pd.concat((perc_p1, perc_p2, perc_temp, perc_hum, perc_press), axis=1)
+    sensors_location = get_sensors_loc(sds_files+bme_files)
+    return sensor_data, sensors_location
+
+
+def get_weather_data(wather_file: str) -> pd.DataFrame:
     """
     Select and resample wather data
     :param wather_file: path to raw data file
@@ -128,10 +147,14 @@ def get_wather_data(wather_file: str) -> pd.DataFrame:
     return meteo_data
 
 
-if __name__ == '__main__':
-    avg_data, sensors = average_sensors(SENSOR_DATA_FOLDER, START_DATE, END_DATE)
-    meteo_data = get_wather_data(os.path.join(WATHER_DATA_FOLDER, WATHER_FILE))
-    for c in meteo_data.columns:
-        avg_data[c] = meteo_data[c]
-    avg_data.to_csv('DATA/processed/dataset.csv')
+def main():
+    avg_data, sensors = get_sensor_data(SENSOR_DATA_FOLDER)
+    avg_data.index = avg_data.index.tz_localize(tz="UTC")
+    meteo_data = get_weather_data(os.path.join(WATHER_DATA_FOLDER, WATHER_FILE))
+    data = pd.concat((avg_data, meteo_data), axis=1)
+    data.to_csv('DATA/processed/dataset.csv')
     sensors.to_csv('DATA/processed/sensors.csv')
+
+
+if __name__ == '__main__':
+    main()
