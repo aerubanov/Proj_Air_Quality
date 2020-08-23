5d87ce9c9405fd7416a88c3f6b2da5d200321e8f
Anatoly Rubanov <anatolijrubanov@gmail.com>
2020-08-23 20:42:17 +0300

    add and fix tests

--- tests/web/test_client_helper_func.py
+++ tests/web/test_client_helper_func.py
@@ -1,18 +1,23 @@
-from src.web.client.application.helper_functions import aqi_level, pm25_to_aqius
+import datetime
+import pandas as pd
+
+from src.web.client.application.helper_functions import aqi_level, pm25_to_aqius,\
+    get_sensor_data, get_anomaly_data, get_forecast_data
+from tests.web.test_web_client import sensor_data, anomaly_data, forec_data
 
 
 def test_aqi_level():
-    assert aqi_level(25) == 'good'
-    assert aqi_level(50) == 'good'
-    assert aqi_level(51) == 'moderate'
-    assert aqi_level(100) == 'moderate'
-    assert aqi_level(101) == 'Unhealthy for sens. groups'
-    assert aqi_level(150) == 'Unhealthy for sens. groups'
-    assert aqi_level(151) == "Unhealthy"
-    assert aqi_level(200) == 'Unhealthy'
-    assert aqi_level(201) == 'Very Unhealthy'
-    assert aqi_level(300) == 'Very Unhealthy'
-    assert aqi_level(301) == 'Hazardous'
+    assert aqi_level(25) == 'green'
+    assert aqi_level(50) == 'green'
+    assert aqi_level(51) == 'gold'
+    assert aqi_level(100) == 'gold'
+    assert aqi_level(101) == 'orange'
+    assert aqi_level(150) == 'orange'
+    assert aqi_level(151) == 'red'
+    assert aqi_level(200) == 'red'
+    assert aqi_level(201) == 'purple'
+    assert aqi_level(300) == 'purple'
+    assert aqi_level(301) == 'brown'
 
 
 def test_pm25_to_aqius():
@@ -22,3 +27,33 @@ def test_pm25_to_aqius():
     assert round(pm25_to_aqius(60)) == 152
     assert round(pm25_to_aqius(160)) == 210
     assert round(pm25_to_aqius(270)) == 316
+
+
+def test_get_sensor_data(requests_mock):
+    requests_mock.get('http://api:8000/sensor_data', text=sensor_data)
+    df = get_sensor_data(datetime.datetime(2020, 4, 7), datetime.datetime(2020, 4, 8))
+    assert len(df) == 3
+    assert 'p1' in df.columns
+    assert 'p2' in df.columns
+
+
+def test_get_anomaly_data(requests_mock):
+    requests_mock.get('http://api:8000/anomaly', text=anomaly_data)
+    df = pd.DataFrame({'date': [datetime.datetime(2020, 4, 7, 0, 40,),
+                                datetime.datetime(2020, 4, 7, 6, 40,),
+                                datetime.datetime(2020, 4, 7, 9, 20,)],
+                       'p1': [1, 2, 3],
+                       'p2': [4, 5, 6]})
+    df = df.set_index('date')
+    anomaly = get_anomaly_data(datetime.datetime(2020, 4, 7), datetime.datetime(2020, 4, 8), df)
+    assert len(anomaly) == 2
+    assert 'p1' in anomaly.columns
+    assert 'cluster' in anomaly.columns
+
+
+def test_get_forecast(requests_mock):
+    requests_mock.get('http://api:8000/forecast', text=forec_data)
+    forec = get_forecast_data()
+    assert len(forec) == 24
+    assert 'p1' in forec.columns
+    assert 'p2' in forec.columns
--- tests/web/test_web_client.py
+++ tests/web/test_web_client.py
@@ -58,61 +58,17 @@ def test_index(test_client):
     assert resp.status_code == 200
 
 
-def test_forecast(test_client):
-    resp = test_client.get('/forecast')
-    assert resp.status_code == 200
-
-
-def test_history_get(test_client):
-    resp = test_client.get('/history')
-    assert resp.status_code == 200
-
-
-def test_history_post(test_client):
+def test_index_post(test_client):
     with test_client:
-        resp = test_client.post('/history', data={'start_date': '2020-04-01', 'end_date': '2020-04-05'})
+        resp = test_client.post('/index', data={'start_date': '2020-04-01', 'end_date': '2020-04-05'})
         assert resp.status_code == 200
         assert flask.session['start_date'] == datetime.datetime(2020, 4, 1).isoformat()
         assert flask.session['end_date'] == datetime.datetime(2020, 4, 5).isoformat()
 
 
-def test_anomalies_get(test_client):
-    resp = test_client.get('/anomalies')
-    assert resp.status_code == 200
-
-
-def test_anomalies_post(test_client):
-    with test_client:
-        resp = test_client.post('/anomalies', data={'start_date': '2020-04-01', 'end_date': '2020-04-05'})
-        assert resp.status_code == 200
-        assert flask.session['start_date'] == datetime.datetime(2020, 4, 1).isoformat()
-        assert flask.session['end_date'] == datetime.datetime(2020, 4, 5).isoformat()
-
-
-def test_graph_sensors(test_client, requests_mock):
-    requests_mock.get('http://api:8000/sensor_data',
-                      text=sensor_data)
-    resp = test_client.get('/graph/sensors')
-    assert resp.status_code == 200
-
-
-def test_graph_aqi(test_client, requests_mock):
-    requests_mock.get('http://api:8000/sensor_data',
-                      text=sensor_data)
-    resp = test_client.get('/graph/aqius')
-    assert resp.status_code == 200
-
-
-def test_graph_forecast(test_client, requests_mock):
-    requests_mock.get('http://api:8000/forecast',
-                      text=forec_data)
-    resp = test_client.get('/graph/forecast')
-    assert resp.status_code == 200
-
-
-def test_graph_anomalies(test_client, requests_mock):
-    requests_mock.get('http://api:8000/sensor_data',
-                      text=sensor_data)
+def test_graph(test_client, requests_mock):
+    requests_mock.get('http://api:8000/sensor_data', text=sensor_data)
+    requests_mock.get('http://api:8000/forecast', text=forec_data)
     requests_mock.get('http://api:8000/anomaly', text=anomaly_data)
-    resp = test_client.get('/graph/anomalies')
+    resp = test_client.get('/graph')
     assert resp.status_code == 200
