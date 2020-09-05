a070d8684065515b4d4247c51ab0b3f353648e8a
Anatoly Rubanov <anatolijrubanov@gmail.com>
2020-09-05 23:36:04 +0300

    add tests for bot

--- tests/web/test_bot.py
+++ tests/web/test_bot.py
@@ -1,21 +1,124 @@
-from src.web.bot.bot import get_concentration, API_HOST
+from src.web.bot.bot import get_concentration, get_forecast, keyboard, \
+    API_HOST, start, button
+from tests.web.data.api_test_data import sensor_data, forec_data
+from src.web.bot.model import User as DbUser
 
-sensor_data = '[{"date":"2020-04-07T12:44:06.614608","humidity":29.21038961038961,"p1":9.795701754385965,"p2":' \
-          '4.26359649122807,"pressure":97008.57545454545,"temperature":14.742727272727276},' \
-          '{"date":"2020-04-07T12:49:10.052534","humidity":29.264605263157897,"p1":10.31646017699115,"p2":' \
-          '4.620530973451329,"pressure":96942.81236842106,"temperature":14.907368421052633},' \
-          '{"date":"2020-04-07T12:54:13.287872","humidity":29.3425641025641,"p1":11.230956521739133,"p2":' \
-          '5.416956521739132,"pressure":97040.5755128205,"temperature":15.036666666666665}]\n'
+from telegram import InlineKeyboardMarkup, Update, Message, Chat, CallbackQuery, User
+import datetime
 
 
 def test_get_concentration(requests_mock):
-    requests_mock.get(API_HOST+'/sensor_data', text=sensor_data)
-    anwser = get_concentration()
-    assert anwser.split()[1] == '11.23'
-    assert anwser.split()[3] == '5.42'
-    assert anwser.split()[5] == '46.80'
-    assert anwser.split()[6] == 'Good'
+    requests_mock.get(API_HOST + '/sensor_data', text=sensor_data)
+    answer = get_concentration()
+    assert answer.split()[1] == '11.23'
+    assert answer.split()[3] == '5.42'
+    assert answer.split()[5] == '46.80'
+    assert answer.split()[6] == 'Good'
 
 
-def test_get_forecast():
-    pass
+def test_get_forecast(requests_mock):
+    requests_mock.get(API_HOST + '/forecast', text=forec_data)
+    answer = get_forecast()
+    assert len(answer.splitlines()) == 25
+
+
+def test_keyboard():
+    kb = keyboard()
+    assert isinstance(kb, InlineKeyboardMarkup)
+    assert len(kb.inline_keyboard) == 2
+    assert len(kb.inline_keyboard[0]) == 2
+    assert len(kb.inline_keyboard[1]) == 2
+
+
+class TestBot:
+    def __init__(self):
+        self.response = None
+        self.markup = None
+
+    def send_message(self, chat_id, text, parse_mode=None, disable_web_page_preview=None,
+                     disable_notification=False, reply_to_message_id=None,
+                     reply_markup=None, timeout=None, **kwargs):
+        self.response = text
+        self.markup = reply_markup
+
+    def answerCallbackQuery(self, callback_query_id, text=None,
+                            show_alert=False, url=None, cache_time=None, timeout=None, **kwargs):
+        pass
+
+    def edit_message_text(self, text, chat_id=None, message_id=None, inline_message_id=None,
+                          parse_mode=None, disable_web_page_preview=None, reply_markup=None,
+                          timeout=None, **kwargs):
+        self.response = text
+        self.markup = reply_markup
+
+
+def create_update(text: str, bot: TestBot, query=None):
+    user = User(id=1, first_name='Anatoly', is_bot=False)
+    message = Message(message_id=1, from_user=user, date=datetime.datetime.utcnow(),
+                      text=text, chat=Chat(id=1, type='chat'), bot=bot)
+
+    if query is not None:
+        cb = CallbackQuery(id=1, from_user=user, chat_instance='chat', data=query, bot=bot, message=message)
+    else:
+        cb = None
+    update = Update(update_id=1, message=message, callback_query=cb)
+    return update
+
+
+def test_start():
+    bot = TestBot()
+    update = create_update('/start', bot)
+    start(update, context=None)
+    assert bot.response == 'Чтобы получать уведомления об измении концентрации частиц в воздухе, подпишитесь на бота'
+    assert isinstance(bot.markup, InlineKeyboardMarkup)
+
+
+def test_button_now(bot_db_session, monkeypatch):
+    bot = TestBot()
+    update = create_update('now', bot, query='now')
+    monkeypatch.setattr('src.web.bot.bot.get_concentration', lambda: 'sensor_values')
+    button(update, None, bot_db_session)
+    assert bot.response == 'sensor_values'
+
+
+def test_button_forecast(bot_db_session, monkeypatch):
+    bot = TestBot()
+    update = create_update('forecast', bot, query='forecast')
+    monkeypatch.setattr('src.web.bot.bot.get_forecast', lambda: 'forecast_values')
+    button(update, None, bot_db_session)
+    assert bot.response == 'forecast_values'
+
+
+def test_button_subscribe(bot_db_session, monkeypatch):
+    bot = TestBot()
+    update = create_update('subscribe', bot, query='subscribe')
+    button(update, None, bot_db_session)
+    assert bot.response == 'Вы подписались на получение уведомлений.'
+
+
+def test_button_subscribe_exist(bot_db_session, monkeypatch):
+    bot = TestBot()
+    update = create_update('subscribe', bot, query='subscribe')
+    user = DbUser(id=1, chat_id=1)
+    bot_db_session.add(user)
+    bot_db_session.commit()
+    button(update, None, bot_db_session)
+    assert bot.response == 'Вы уже подписаны.'
+
+
+def test_button_unsubscribe(bot_db_session, monkeypatch):
+    bot = TestBot()
+    update = create_update('unsubscribe', bot, query='unsubscribe')
+    user = DbUser(id=1, chat_id=1)
+    bot_db_session.add(user)
+    bot_db_session.commit()
+    button(update, None, bot_db_session)
+    assert bot.response == 'Вы отписались от получения уведомлений.'
+
+
+def test_button_unsubscribe_not_exist(bot_db_session, monkeypatch):
+    bot = TestBot()
+    update = create_update('unsubscribe', bot, query='unsubscribe')
+
+    button(update, None, bot_db_session)
+    assert bot.response == 'Вы не подписаны.'
