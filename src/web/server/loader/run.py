3d93e6c79bff64b8c35021dfd590932633ecae2c
aerubanov <anatolijrubanov@gmail.com>
2020-12-17 15:11:44 +0300

    more fixes

--- src/web/server/loader/run.py
+++ src/web/server/loader/run.py
@@ -5,10 +5,9 @@ import schedule
 import logging.config
 
 from src.web.server.common.model import Base
-from src.web.server.loader.config import sensor_time_interval
-from src.web.logger.logging_config import LOGGING_CONFIG
+from src.web.server import config
+from src.web.server.logging_config import LOGGING_CONFIG
 from src.web.server.loader.application.tasks import sensor_task, weather_task, mosecom_task
-from src.web.config import DATABASE
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger('LoaderLogger')
@@ -16,11 +15,11 @@ logger = logging.getLogger('LoaderLogger')
 
 if __name__ == '__main__':
     time.sleep(5)  # wait postgres image loading
-    engine = create_engine(DATABASE)
+    engine = create_engine(config.dbstring)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     sess = Session()
-    schedule.every(sensor_time_interval).minutes.do(sensor_task, session=sess, logger=logger, metrics=True)
+    schedule.every(config.loader.sensortimeinterval).minutes.do(sensor_task, session=sess, logger=logger, metrics=True)
     schedule.every().hour.at(":30").do(weather_task, session=sess, logger=logger, metrics=True)
     schedule.every().hour.at(":16").do(mosecom_task, logger=logger, metrics=True)
     logger.info('%s', 'loader started')
--- src/web/server/ml/run.py
+++ src/web/server/ml/run.py
@@ -7,14 +7,14 @@ import logging.config
 from src.web.server.ml.application.forecast import perform_forecast
 from src.web.server.ml.application.anomaly import perform_anomaly_detection
 from src.web.server.common.model import Base
-from src.web.logger.logging_config import LOGGING_CONFIG
-from src.web.config import DATABASE
+from src.web.server.logging_config import LOGGING_CONFIG
+from src.web.server import config
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger('MLLogger')
 
 if __name__ == '__main__':
-    engine = create_engine(DATABASE)
+    engine = create_engine(config.dbstring)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     sess = Session()
