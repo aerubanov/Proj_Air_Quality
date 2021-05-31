b99fe48688b6f25073fc2b0c601e9095374f07ea
aerubanov <anatolijrubanov@gmail.com>
2021-05-31 18:33:49 +0300

    add model training

--- experiments/spatio-temporal.py
+++ experiments/spatio-temporal.py
@@ -1,17 +1,20 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import QuantileTransformer
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import gpflow
 
 
 data_file = 'DATA/processed/dataset.csv'
 
 
-def convert_x(data):
+def convert_time(data):
     data['timestamp'] = pd.to_datetime(data['timestamp'])
-    data = data[['timestamp', 'lon', 'lat']]
     data['timestamp'] = (
             data['timestamp'] - pd.to_datetime('2021-01-01', utc=True)
             )/pd.Timedelta(hours=1)
-    return data.values
+    return data
 
 
 def get_data(file_path: str):
@@ -24,18 +27,101 @@ def get_data(file_path: str):
             )
     qt.fit(data.spat.y)
 
-    data = data.spat.tloc['2021-01-01':'2021-02-01']
+    data = data.spat.tloc['2021-01-01':'2021-01-04']
     data = data.dropna()
-    train_data = data.spat.tloc['2021-01-01':'2021-01-29']
-    test_data = data.spat.tloc['2021-01-29':]
-    y = train_data.spat.y.values
-    y = qt.transform(y).flatten()
-    y_test = test_data.spat.y.values
-    y_test = qt.transform(y_test).flatten()
-    x = convert_x(train_data)
-    x_test = convert_x(test_data)
-    return x, y, x_test, y_test
+    data = data[['timestamp', 'lat', 'lon', 'P1', 'sds_sensor']]
+    train_data = data.spat.tloc['2021-01-01':'2021-01-03']
+    test_data = data.spat.tloc['2021-01-03':]
+
+    # TODO replace by data.spat.y after solving issue 120
+    train_data['P1'] = qt.transform(train_data.spat.y.values).flatten()
+    test_data['P1'] = qt.transform(test_data.spat.y.values).flatten()
+
+    train_data = convert_time(train_data)
+    test_data = convert_time(test_data)
+
+    return train_data, test_data
+
+
+def train_model(data: pd.DataFrame, M=200) -> gpflow.models.sgpr.SGPR:
+    x = data[['timestamp', 'lon', 'lat']].values
+    y = data['P1'].values[:, None]
+
+    spat_cov = gpflow.kernels.Matern32(
+            variance=1,
+            lengthscales=0.1,
+            active_dims=[1, 2],
+            )
+
+    mt = gpflow.kernels.Matern32(variance=1, lengthscales=24, active_dims=[0])
+    pk = gpflow.kernels.Periodic(
+            gpflow.kernels.SquaredExponential(
+                lengthscales=24*1.5,
+                active_dims=[0],
+                ),
+            period=24,
+            )
+    time_cov = mt * pk
+
+    cov = spat_cov * time_cov
+
+    Z = x[np.random.permutation(x.shape[0])[0:M], :]
+    model = gpflow.models.sgpr.SGPR((x, y), cov, Z)
+
+    optimizer = gpflow.optimizers.Scipy()
+    optimizer.minimize(
+            model.training_loss,
+            model.trainable_variables,
+            options={'disp': False},
+            )
+    return model
 
 
 if __name__ == '__main__':
-    x, y, x_test, y_test = get_data(data_file)
+    df_train, df_test = get_data(data_file)
+    print(df_train)
+    print(df_test)
+    # x = df_train[np.isclose(df_train['timestamp'], 13.0)]
+    # print(x)
+    # plt.scatter(x['lon'], x['lat'], c=x['P1'], cmap='rainbow')
+    # plt.colorbar()
+    # plt.show()
+    # x1 = df_train.spat.random_sensors(1)
+    # plt.plot(x1['timestamp'], x1['P1'])
+    # plt.show()
+    model = train_model(df_train, M=400)
+    gpflow.utilities.print_summary(model)
+
+    x_test = df_test[['timestamp', 'lon', 'lat']].values
+    y_test = df_test['P1'].values
+    mu, var = model.predict_f(x_test)
+    mse = mean_squared_error(y_test, mu[:, 0])
+    print(mse)
+
+    x_test = df_test[df_test['timestamp'] == 62.0][[
+        'timestamp', 'lon', 'lat'
+        ]].values
+    y_test = df_test[df_test['timestamp'] == 62.0]['P1'].values
+    lon_min, lon_max = x_test[:, 1].min(), x_test[:, 1].max()
+    lat_min, lat_max = x_test[:, 2].min(), x_test[:, 2].max()
+
+    new_lon, new_lat = np.mgrid[lon_min:lon_max:0.025, lat_min:lat_max:0.0125]
+    xx = np.vstack((55 * np.ones(new_lon.flatten().shape[0]),
+                    new_lon.flatten(),
+                    new_lat.flatten())).T
+    print(xx)
+    mx, _ = model.predict_f(xx)
+
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8))
+    ax1.hexbin(xx[:, 1], xx[:, 2], mx,
+               gridsize=30, cmap='rainbow', alpha=0.7)
+    plot = ax1.scatter(x_test[:, 1], x_test[:, 2],
+                       c=y_test,
+                       s=300, cmap='rainbow')
+    ax2.scatter(x_test[:, 1], x_test[:, 2],
+                c=y_test,
+                s=300, cmap='rainbow')
+    ax1.set_title("Data + Posterior")
+    ax2.set_title("Data")
+    plt.colorbar(plot)
+    plt.show()
