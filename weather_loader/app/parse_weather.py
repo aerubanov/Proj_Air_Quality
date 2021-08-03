89dd4f923e8c6fad15f1abae6db37853be89db88
Anatoly <anatolijrubanov@gmail.com>
2021-08-03 19:16:08 +0300

    add docker files

--- weather_loader/app/parse_weather.py
+++ weather_loader/app/parse_weather.py
@@ -116,7 +116,7 @@ max_col = 24
 url = "https://rp5.ru/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_" \
         "%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B5_(%D1%86%D0%B5%D0%BD%D1%82" \
         "%D1%80,_%D0%91%D0%B0%D0%BB%D1%87%D1%83%D0%B3)"
-filename = 'parsed_weather.csv'
+filename = 'DATA/raw/forecast/parsed_weather.csv'
 # Schedule
 if __name__ == '__main__':
     schedule.every().hour.at(":00").do(start_parsing, url=url)
